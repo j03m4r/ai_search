4e12fa69d25bdc6c457fad1ad8843bf2e585e18b
Joel Markley <joeemarr12345@gmail.com>
2026-07-31 14:04:29 -0500

    Changing color of 'I do not consent' button to gray'

--- src/components/setup/ConsentForm.vue
+++ src/components/setup/ConsentForm.vue
@@ -270,7 +270,7 @@
   <div
     class="shadow flex justify-center items-center border p-1 rounded-2xl bg-white border-gray-300 mb-12! mt-6! gap-x-2">
     <a href="https://app.prolific.com/submissions/complete?cc=C12UWYZ2"
-      class="px-6 py-6 bg-indigo-500 text-white rounded-xl flex gap-x-2 justify-center items-center text-lg cursor-pointer hover:bg-indigo-600 transition-colors">
+      class="px-6 py-6 bg-gray-200 text-black rounded-xl flex gap-x-2 justify-center items-center text-lg cursor-pointer hover:bg-gray-300 transition-colors">
       I do not consent
     </a>
     <button @click="emit('nextStep')"

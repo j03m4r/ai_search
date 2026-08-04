bd4df32b563981901190af909886380d5f3611f5
Joel Markley <joeemarr12345@gmail.com>
2026-08-04 15:40:02 -0500

    Adding reminder before starting search task for participants to expand and change their concept map as they search

--- src/components/setup/InitialRepresentation.vue
+++ src/components/setup/InitialRepresentation.vue
@@ -35,9 +35,23 @@
         representation.value = categories;
         emit('updateInitialRepresentation', representation.value)
     }
+
+    const startSearchTask = () => {
+        pendingStart.value = true;
+    }
+
+    const pendingStart = ref(false);
 </script>
 
 <template v-else-if="step === 5">
+    <div class="w-screen h-screen fixed top-0 left-0 bg-black/50 flex items-center justify-center z-100" v-if="pendingStart">
+        <div class="bg-white p-4 rounded-xl shadow-lg w-lg flex flex-col justify-center items-start gap-y-6!">
+            <p class="text-xl font-semibold!">You are about to start the search task.</p>
+            <p class="text-xl">Remember, you should build out, expand, and change your initial concept map to accommodate for new factors you discover while searching.</p>
+            <button @click="emit('begin')" class="cursor-pointer w-full bg-indigo-500 text-white px-4 py-2 rounded-lg hover:bg-indigo-600 transition-colors">Start search task</button>
+        </div>
+    </div>
+
     <div class="sticky top-0 w-full flex justify-start items-center bg-[#faf9f6] py-6 z-50">
         <h2 class="text-3xl font-bold! sticky top-0 bg-[#faf9f6] text-gray-600">Your Initial Understanding</h2>
     </div>
@@ -76,7 +90,7 @@
         <div class="mb-40!"></div>
         <div
             class="shadow fixed bottom-12 flex justify-center items-center border p-1 rounded-2xl bg-white border-gray-300">
-            <button @click="emit('begin')" :disabled="!validToBegin"
+            <button @click="startSearchTask" :disabled="!validToBegin"
                 class="px-6 py-6 bg-indigo-500 disabled:bg-gray-300 disabled:text-gray-500 disabled:cursor-not-allowed text-white rounded-xl flex gap-x-2 justify-center items-center text-lg cursor-pointer hover:bg-indigo-600 transition-colors">
                 Begin Search Task
             </button>
